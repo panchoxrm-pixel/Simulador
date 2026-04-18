f13b88cde61b794a797c50953b59857d7ca4f01c
Francisco Rodríguez <panchoxrm@gmail.com>
2026-04-17 23:46:58 -0500

    feat: Crear las funciones de cálculo e invocarlas en html y en simulador.js

--- funciones.js
+++ funciones.js
@@ -1,15 +1,41 @@
 
 function calcularDisponible(ingresos,egresos){
-    let valorDisponible=0;
+    let valorDisponible;
     valorDisponible=ingresos-egresos;
     if(valorDisponible<0){
-        return "0";
-    }
-    return valorDisponible;
-    }
+        return 0;
+    } return valorDisponible;
+}
 
 function calcularCapacidadPago(montoDisponible){
     let capacidadPago;
     capacidadPago=montoDisponible/2;
     return capacidadPago;
+}
+
+function calcularInteresSimple(monto,tasa,plazoAños){
+    let interesSimple;
+    interesSimple=monto*(tasa/100)*plazoAños;
+    return interesSimple;
+}
+
+function calcularTotalPagar(monto,interes){
+    let totalPagar;
+    let impuestoSolca=100;
+    totalPagar=monto+interes+impuestoSolca;
+    return totalPagar;
+}
+
+function calcularCuotaMensual(totalPagar,plazoAños){
+    let cuotaMensual;
+    cuotaMensual=totalPagar/(plazoAños*12);
+    return cuotaMensual;
+}
+
+function aprobarCredito(capacidadPago,cuotaMensual){
+    if(capacidadPago>cuotaMensual){
+       return true;
+    } else {
+        return false
+    }
 }
--- simulador.js
+++ simulador.js
@@ -1,16 +1,107 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
 function calcular(){
-    let ingresosFloat=0;
-    let egresosFloat=0;
-    let cmpIngresosFloat;
-    let cmpEgresosFloat;
+    let ingresos;
+    let egresos;
+    let cmpIngresos;    
+    let cmpEgresos;
+    let ingresosFloat;
+    let egresosFloat;  
     let valorDisponibleFloat;
+    let lblDisponibleValor;
+    let lblCapacidadValor;
+    let monto;
+    let cmpMonto;
+    let montoEntero;
+    let plazoAños;
+    let cmpPlazoAños;
+    let plazoAñosEntero;
+    let tasa;
+    let cmpTasa;
+    let tasaEntero;
+    let lblInteresAPagar;
+    let totalPagar;
+    let lblTotalValor;
+    let lblCuotaValor;
+    let mensaje;
+    let estadoCredito;
 
-    cmpIngresosFloat=document.getElementById("txtIngresos");
-    cmpIngresosFloat=document.getElementById("txtEngresos");
-    cmpIngresosFloat=(cmp.ingresos.value);
-    cmpEgresosFloat=(cmp.egresos.value);
-    valorDisponibleFloat= calcularDisponible(ingresosFloat,egresosFloat);
-    total=document.getElementById("spnDisponible");
-    total.innerText=valorDisponibleFloat;
+    // ¿Cómo convertir un string (cadena de texto) en un float (número decimal) y cómo guardarlo?
+    // 1. Recuperar la id (en html) de la caja de texto de ingresos y asignarle a una variable
+    // 2. Extraer de la variable anterior el dato (string) de la caja de texto y guardarlo en otra variable.
+    // 3. Convertir en # decimal (float) el string extraído con la función parseFloat 
+
+    cmpIngresos=document.getElementById("txtIngresos");
+    ingresos=cmpIngresos.value;
+    ingresosFloat=parseFloat(ingresos);
+
+    cmpEgresos=document.getElementById("txtEgresos");
+    egresos=cmpEgresos.value;
+    egresosFloat=parseFloat(egresos);
+    
+    valorDisponibleFloat=calcularDisponible(ingresosFloat,egresosFloat);
+
+    lblDisponibleValor=document.getElementById("spnDisponible");
+    lblDisponibleValor.innerText=valorDisponibleFloat;
+
+    capacidadPago=calcularCapacidadPago(valorDisponibleFloat);
+
+    lblCapacidadValor=document.getElementById("spnCapacidadPago");
+    lblCapacidadValor.innerText=capacidadPago;
+
+    cmpMonto=document.getElementById("txtMonto");
+    monto=cmpMonto.value;
+    montoEntero=parseInt(monto);
+
+    cmpTasa=document.getElementById("txtTasaInteres");
+    tasa=cmpTasa.value;
+    tasaEntero=parseInt(tasa);
+
+    cmpPlazoAños=document.getElementById("txtPlazo");
+    plazoAños=cmpPlazoAños.value;
+    plazoAñosEntero=parseInt(plazoAños);
+
+    interesSimple=calcularInteresSimple(montoEntero,tasaEntero,plazoAñosEntero);
+
+    lblInteresAPagar=document.getElementById("spnInteresPagar");
+    lblInteresAPagar.innerText=interesSimple.toFixed(2);
+
+    totalPagar=calcularTotalPagar(montoEntero,interesSimple);
+
+    lblTotalValor=document.getElementById("spnTotalPrestamo");
+    lblTotalValor.innerText=totalPagar.toFixed(2);
+
+    cuotaMensual=calcularCuotaMensual(totalPagar,plazoAñosEntero);
+
+    lblCuotaValor=document.getElementById("spnCuotaMensual");
+    lblCuotaValor.innerText=cuotaMensual.toFixed(2);
+    
+
+    analisisCredito=aprobarCredito(capacidadPago,cuotaMensual);
+        
+        if(analisisCredito==true){
+            estadoCredito="Crédito aprobado"
+        } else {
+            estadoCredito="Crédito rechazado"
+        }
+        
+        mensaje=document.getElementById("spnEstadoCredito");
+        mensaje.innerText=estadoCredito;
 }
+
+function reiniciar() {
+    document.getElementById("txtIngresos").value = "";
+    document.getElementById("txtEgresos").value = "";
+    document.getElementById("txtMonto").value = "";
+    document.getElementById("txtPlazo").value = "";
+    document.getElementById("txtTasaInteres").value = "";
+    document.getElementById("spnDisponible").innerText = "";
+    document.getElementById("spnCapacidadPago").innerText = "";
+    document.getElementById("spnInteresPagar").innerText = "";
+    document.getElementById("spnTotalPrestamo").innerText = "";
+    document.getElementById("spnCuotaMensual").innerText = "";
+    document.getElementById("spnEstadoCredito").innerText = "ANALIZANDO...";
+    document.getElementById("spnEstadoCredito").style.color = "black";
+}
+
+       
+    
